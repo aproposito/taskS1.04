d68c3fce0d40862a1befc5f9aeb630c5f6fd2a97
Alvaro <aproposito@gmail.com>
2026-07-06 10:51:03 +0200

    fix redundant contructor

--- TaskS1_04_L1_POO_2/src/Rectangle.php
+++ TaskS1_04_L1_POO_2/src/Rectangle.php
@@ -2,9 +2,7 @@
 declare(strict_types=1);
 
 class Rectangle extends Shape {
-    public function __construct(float $height, float $width){
-         parent:: __construct($height, $width);
-    }
+
 public function area() : float {
     return $this->getHeight() * $this->getWidth();
 }
